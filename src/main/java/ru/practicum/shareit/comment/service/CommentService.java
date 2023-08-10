1610a9d1e731aab33f9e4c7ce7b6d24f1666fca8
lemfy <117842031+lemfy@users.noreply.github.com>
2023-08-11 01:56:26 +0300

    refactoring after loose

--- src/main/java/ru/practicum/shareit/comment/service/CommentService.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.comment.service;
-
-import ru.practicum.shareit.comment.dto.CommentDto;
-import ru.practicum.shareit.item.dto.ItemDto;
-
-public interface CommentService {
-    ItemDto addComment(Integer authorId, Integer itemId, CommentDto commentDto);
-}
