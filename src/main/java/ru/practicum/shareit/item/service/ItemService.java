651e7590f805ab88b38ea811c79990bb020e17ff
lemfy <117842031+lemfy@users.noreply.github.com>
2023-08-21 05:27:46 +0300

    Add files via upload

---
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.item.service;
+
+import ru.practicum.shareit.item.dto.CommentRequestDto;
+import ru.practicum.shareit.item.dto.CommentResponseDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.util.List;
+
+public interface ItemService {
+    ItemDto createItem(Integer userId, ItemDto itemDto);
+
+    ItemDto getItem(Integer userId, Integer itemId);
+
+    List<ItemDto> getAllItems(Integer userId);
+
+    ItemDto updateItem(Integer id, Integer itemId, ItemDto itemDto);
+
+    void deleteItem(Integer userId, Integer itemId);
+
+    List<ItemDto> searchItems(String text);
+
+    CommentResponseDto addComment(Integer userId, Integer itemId, CommentRequestDto text);
+}
