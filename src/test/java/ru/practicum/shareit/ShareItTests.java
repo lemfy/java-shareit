3726a15ac65a03ffc7ad4fc594f32a5351d2cc66
lemfy <117842031+lemfy@users.noreply.github.com>
2023-08-21 05:06:01 +0300

    Add files via upload

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
 class ShareItTests {
