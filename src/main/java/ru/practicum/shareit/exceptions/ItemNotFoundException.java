5e4c7dfd02cdf72d38da911b564e66157e0bb154
lemfy <117842031+lemfy@users.noreply.github.com>
2023-08-21 18:51:15 +0300

    Delete src directory

--- src/main/java/ru/practicum/shareit/exceptions/ItemNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-import javax.persistence.EntityNotFoundException;
-
-public class ItemNotFoundException extends EntityNotFoundException {
-    public ItemNotFoundException(Long id) {
-        super(String.format("Вещь не найдена: %d ", id));
-    }
-}
