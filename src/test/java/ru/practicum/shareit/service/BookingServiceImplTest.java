74feddd000d1b865a373a0fbd1498b11d3bd287a
lemfy <117842031+lemfy@users.noreply.github.com>
2023-08-21 04:51:38 +0300

    Add files via upload

--- src/test/java/ru/practicum/shareit/service/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/service/BookingServiceImplTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
--- src/test/java/ru/practicum/shareit/service/ItemRequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/service/ItemRequestServiceImplTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.shareit.exceptions.UserNotFoundException;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
