a19c1b3f8a0a7f45a5e71698460283acc69b9977
lemfy <117842031+lemfy@users.noreply.github.com>
2023-08-21 06:32:58 +0300

    Delete src directory

--- src/main/java/ru/practicum/shareit/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-import javax.persistence.EntityNotFoundException;
-
-public class UserNotFoundException extends EntityNotFoundException {
-    public UserNotFoundException(Integer id) {
-        super(String.format("Пользователь с id [%d] не найден!", id));
-    }
-}
