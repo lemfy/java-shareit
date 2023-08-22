3ae438ed84ae4e29b8e7ad781a6a2ef5a0e1ece1
lemfy <117842031+lemfy@users.noreply.github.com>
2023-08-22 15:00:45 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.user.dto;
 import lombok.*;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
