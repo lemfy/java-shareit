a3bacf79054e2d44fd837ecb12914739ea873856
lemfy <117842031+lemfy@users.noreply.github.com>
2023-10-02 18:25:25 +0300

    server fixes

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItServer { public static void main(String[] args) {
+public class ShareItServer {
+	public static void main(String[] args) {
 		SpringApplication.run(ShareItServer.class, args);
 	}
 }
