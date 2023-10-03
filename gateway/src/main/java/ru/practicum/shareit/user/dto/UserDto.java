0ed2a79c1be5494e67c1f12e06d29cb40b7b22c0
lemfy <117842031+lemfy@users.noreply.github.com>
2023-10-03 14:42:07 +0300

    Add files via upload

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
